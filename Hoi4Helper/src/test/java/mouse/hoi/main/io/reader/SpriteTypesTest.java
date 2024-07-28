ebc972549f9eee10003b5838763bfe0d676177f8
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-07-28 16:25:58 +0300

    improve test

--- Hoi4Helper/src/test/java/mouse/hoi/main/io/reader/SpriteTypesTest.java
+++ Hoi4Helper/src/test/java/mouse/hoi/main/io/reader/SpriteTypesTest.java
@@ -6,6 +6,7 @@ import mouse.hoi.tools.parser.impl.ast.AbstractSyntaxTree;
 import mouse.hoi.tools.parser.impl.bind.ScanParseBinder;
 import mouse.hoi.tools.parser.impl.reader.engine.ReaderEngine;
 import mouse.hoi.tools.parser.impl.writer.engine.WriterEngine;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,45 +25,45 @@ class SpriteTypesTest {
 
     @Test
     void readWriteSimple() {
-        String content = """
+
+        String expected = """
                 spriteTypes = {
-                	#### Generic continuous goal icons
                 	SpriteType = {
                 		name = "GFX_focus_ent_leyla_athella_shine"
                 		texturefile = "gfx/interface/goals/focus_ent_leyla_athella.dds"
                 		effectFile = "gfx/FX/buttonstate.lua"
                 		animation = {
                 			animationmaskfile = "gfx/interface/goals/focus_ent_leyla_athella.dds"
-                			animationtexturefile = "gfx/interface/goals/shine_overlay.dds" 	# <- the animated file
-                			animationrotation = -90.0		# -90 clockwise 90 counterclockwise(by default)
-                			animationlooping = no			# yes or no ;)
-                			animationtime = 0.75				# in seconds
-                			animationdelay = 0			# in seconds
-                			animationblendmode = "add"       #add, multiply, overlay
-                			animationtype = "scrolling"      #scrolling, rotating, pulsing
+                			animationtexturefile = "gfx/interface/goals/shine_overlay.dds"
+                			animationrotation = -90.000
+                			animationlooping = no
+                			animationtime = 0.750
+                			animationdelay = 0.000
+                			animationblendmode = "add"
+                			animationtype = "scrolling"
                 			animationrotationoffset = { x = 0.0 y = 0.0 }
                 			animationtexturescale = { x = 1.0 y = 1.0 }
                 		}
                 		animation = {
                 			animationmaskfile = "gfx/interface/goals/focus_ent_leyla_athella.dds"
-                			animationtexturefile = "gfx/interface/goals/shine_overlay.dds" 	# <- the animated file
-                			animationrotation = 90.0		# -90 clockwise 90 counterclockwise(by default)
-                			animationlooping = no			# yes or no ;)
-                			animationtime = 0.75				# in seconds
-                			animationdelay = 0			# in seconds
-                			animationblendmode = "add"       #add, multiply, overlay
-                			animationtype = "scrolling"      #scrolling, rotating, pulsing
+                			animationtexturefile = "gfx/interface/goals/shine_overlay.dds"
+                			animationrotation = 90.000
+                			animationlooping = no
+                			animationtime = 0.750
+                			animationdelay = 0.000
+                			animationblendmode = "add"
+                			animationtype = "scrolling"
                 			animationrotationoffset = { x = 0.0 y = 0.0 }
                 			animationtexturescale = { x = 1.0 y = 1.0 }
                 		}
                 		legacy_lazy_load = no
                 	}
                 }
+                
                 """;
-        AbstractSyntaxTree ast = binder.createTreeFromContent(content);
+        AbstractSyntaxTree ast = binder.createTreeFromContent(expected);
         SpriteTypesWrapper fromContent = reader.read(ast, SpriteTypesWrapper.class);
-        System.out.println(fromContent);
         String backToString = writer.write(fromContent);
-        System.out.println(backToString);
+        Assertions.assertEquals(expected, backToString);
     }
 }
